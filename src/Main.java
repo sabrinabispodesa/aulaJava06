42d126e4d486bfb645abc5ca0eefc6403b4b1efb
Sabrina  de Sá <sabrinabispodesa@gmail.com>
2025-07-21 23:33:55 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
-public class Main {
+public class Aula06 {
+
     public static void main(String[] args) {
- //   Scanner scanner =  new Scanner(System.in);
-        //
-        Pessoa p1 = new Pessoa();
-        p1.setNome("ana");
-        p1.setIdade(25);
-        //
-        Pessoa p2= new Pessoa();
-        p2.setNome("Minoru");
-        p2.setIdade(25);
+        
+        Pessoa pessoa = new Pessoa("Sabrina", 10, "Professora");
+        //String brohibitted = pessoa.letraProibida();
+        
+         // System.out.println(brohibitted);
+        
+         
+     
+        
+        System.out.println(" " + "O nome é " + pessoa.getNome() + "\n " +  "A idade é " + pessoa.getIdade() + "\n " + "A profissão é " + pessoa.getProfissao()+ "\n ");
+        
+        pessoa.idadeErrada();
+        pessoa.letraProibida();
+        pessoa.temIdade();
 
-       Pessoa p3= new Pessoa();
-       p3.setNome("Sabrina");
-       p3.setIdade(25);
-       p3.setProfissao("Professora");
-
-        //
-
-
-
-
-        //
-        System.out.println("Nome: " + p1.getNome()+ "\n");
-        System.out.println("Idade: " + p1.getIdade()+"\n");
-        System.out.println("Nome: " + p2.getNome()+"\n");
-        System.out.println("Idade: " + p2.getIdade()+"\n");
-        System.out.println("Nome: " + p3.getNome()+"\n");
-        System.out.println("Idade: " + p3.getIdade()+"\n");
-        System.out.println("Profissão:" + p3.getProfissao() );
     }
+    
+    
+    
+    
+
+    
 }

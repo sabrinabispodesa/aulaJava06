efa4bf71339d1b3ad99be2ebd59a58c9df40ecb9
Sabrina  de Sá <sabrinabispodesa@gmail.com>
2025-07-22 16:33:54 -0300

    Update Main.java

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,14 +1,40 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Agenda agenda = new Agenda();
 
-        agenda.setNome("Sabrina");
-        agenda.setEmail("sabrinabispodesa@gmail.com");
-        agenda.setDdTelefone(11);
-        agenda.setTelefone(917320837);
-        System.out.println("\n" + agenda.getNome() + "\n" + agenda.getEmail() + "\n" + agenda.getDdTelefone() + " " + agenda.getTelefone());
+
+        System.out.println("\n" + "------- Cadastro de informações na agenda -------" + "\n");
+
+        System.out.println("Digite o seu nome: ");
+        agenda.setNome(scanner.next());
+
+        System.out.println("Digite o seu email: ");
+        agenda.setEmail(scanner.next());
+
+        do {
+            agenda.characterNecessario();
+            agenda.setEmail(scanner.next());
+        }while (agenda.getEmail().equalsIgnoreCase("@"));
+
+        System.out.println("Digite o seu DD: ");
+        agenda.setDdTelefone(scanner.nextInt());
+
+        if (agenda.getDdTelefone() == 11) {
+            System.out.println("\n"+ "O seu número é de São Paulo"+"\n");
+        } else {
+            System.out.println("Não é possível cadastrar número!");
+            System.exit(agenda.getDdTelefone());
+
+        }
+
+        System.out.println("Digite o seu número sem DD");
+        agenda.setTelefone(scanner.nextInt());
+
+
+        agenda.mostrarInfo();
 
 
 

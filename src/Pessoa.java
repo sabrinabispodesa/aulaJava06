935038f718cd158a3929e87351a99ecc4f075147
Sabrina  de Sá <sabrinabispodesa@gmail.com>
2025-07-21 23:33:10 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -3,52 +3,67 @@ public class Pessoa {
     private String nome;
     private int idade;
     private String profissao;
-
-    // Getter do nome
-//    public String getNome() {
-//        // return nome;
-//    }
-
-    public String getNome() {
-        if (nome.toLowerCase().contains("y")) {
-            System.out.println("Nome inválido (contém a letra 'y').");
-            return null;
-        }
-        return nome;
-    }
-
-    // Setter do nome
-    public void setNome(String nome) {
-        // nome = nome
+    
+    // Constructor is a way of defining  an object and give values to the proporties of that object.
+    
+    public Pessoa(String nome, int idade, String professora){
         this.nome = nome;
+        this.idade = idade;
+        this.profissao = professora;
+    
     }
-
-    // Getter da idade
-    public int getIdade() {
-        return idade;
+    
+    
+    public String getNome(){
+    return this.nome;
     }
-
-    public void setProfissao(String profissao){
-        this.profissao = profissao;
-
+    
+    public void setNome(String nome){
+    this.nome = nome; 
     }
-
+    
+     public int getIdade(){
+    return this.idade;
+    }
+    
+    public void setIdade(int idade){
+    this.idade = idade; 
+    }
+    
+     
     public String getProfissao(){
-        if(getIdade() <=16){
-            System.out.println("Não tem profissão");
-        }else {
-        }  return profissao;
+    return this.profissao;
     }
-
-
-
-    // Setter da idade
-    public void setIdade(int idade) {
-        if (idade >= 25) {
-            this.idade = idade;
-        } else {
-            System.out.println("Idade inválida.");
-        }
-
-        }
+    
+    public void setProfissao(String profissao){
+    this.profissao = profissao; 
     }
+    
+    public String letraProibida(){
+    if(nome.toLowerCase().contains("y")){
+        System.out.println("Nome inválido (contém letra 'y').");
+        return null;
+    } 
+        return nome;
+    } 
+    
+    public int idadeErrada(){
+    if (getIdade() >=18){
+        System.out.println("Idade válida");    
+    }else {
+        System.out.println("Idade Inválida");     
+    } return getIdade();
+       
+}
+    public String temIdade(){
+    if(getIdade() >=18){
+        System.out.println("Tem profissão");
+    }else{
+        System.out.println("Não tem profissão");
+        
+        return null;
+    }
+        return profissao;
+    }
+}
+
